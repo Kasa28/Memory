46265c965a61967c982df23e39118c08a63602ce
Kasa28 <agliullinaksenija@gmail.com>
2026-04-16 13:44:59 +0200

    added section and new function for card

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import "./styles/style.scss";
 
-document.getElementById("h1_memory")!.innerHTML = "Memory Game";
+init()
+
+function init() {
+    const fieldRef = document.getElementById("field")
+    if (fieldRef) {
+        fieldRef.addEventListener("click", e => {
+            const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement
+            if (card) {
+                card.classList.toggle("is-flipped")
+            }
+        })
+    }
+}
